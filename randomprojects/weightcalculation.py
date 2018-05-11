f3441c673c5b8dcc7323863d4e235ef26c450534
Sara Rabiee <sara@Saras-MacBook-Pro.local>
2018-05-11 17:31:18 +0200

    Fixes for the pullrequest

--- randomprojects/weightcalculation.py
+++ randomprojects/weightcalculation.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-mx_week_weight = np.matrix('0.65, 0.75, 0.85; 0.70, 0.80, 0.90; 0.75, 0.85, 0.95')
-max_weight = int(input('How many kilogram was your latest max for squat training?(e.g. 52.5)'))
+mx_week_weight = np.matrix('0.65, 0.75, 0.85; 0.70, 0.80, 0.90; 0.75, 0.85, 0.95; 0.40, 0.50, 0.60')
+max_weight = int(input('How many kilogram(s) was your latest max for squat training?(e.g. 52.5)'))
 
 
 first_month_weight = np.dot(mx_week_weight, max_weight)
