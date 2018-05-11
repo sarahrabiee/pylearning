fcc4fc5479b39fce2514e4df784272511a108311
Sara Rabiee <sara@Saras-MacBook-Pro.local>
2018-05-11 16:28:27 +0200

    this script can calculate the weekly squat weight for the 1st month.

---
+++ randomprojects/weightcalculation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+mx_week_weight = np.matrix('0.65, 0.75, 0.85; 0.70, 0.80, 0.90; 0.75, 0.85, 0.95')
+max_weight = 52.5
+
+first_month_weight = np.dot(mx_week_weight, max_weight)
+
+print(mx_week_weight)
+print('------weights----------')
+print(first_month_weight)
