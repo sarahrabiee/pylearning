e0e48327677b245ea9f7b1bd18cbe7a525322163
Sara Rabiee <sara@Saras-MacBook-Pro.local>
2018-05-11 17:17:59 +0200

    Gets input for the maximum weight

--- randomprojects/weightcalculation.py
+++ randomprojects/weightcalculation.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 mx_week_weight = np.matrix('0.65, 0.75, 0.85; 0.70, 0.80, 0.90; 0.75, 0.85, 0.95')
-max_weight = 52.5
+max_weight = int(input('How many kilogram was your latest max for squat training?(e.g. 52.5)'))
+
 
 first_month_weight = np.dot(mx_week_weight, max_weight)
 
