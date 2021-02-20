f957d04e5e3dd29c1e265007e1dba0777283aafe
sara Rabiee <sarahrabiee@gmail.com>
2021-02-20 23:00:37 +0100

    The first exercise about reversing a string and if condition is done.

--- exercises/02-greeting.py
+++ exercises/02-greeting.py
@@ -1,8 +1,12 @@
 import sys
 
 def hello(name):
-    # TODO: Implement this correctly according to the exercise
-    print(name)
+    if name == "there":
+        print("Hello yourself!")
+    else:
+        reversed_name = ''.join(reversed(name))
+        print("Hello " + reversed_name + "!")
+
 
 if __name__ == '__main__':
     name = sys.argv[1] if len(sys.argv) > 1 else 'there'
