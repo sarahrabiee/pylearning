659ca7ffc941c1ebbd90d4a5071cc4258398bc5e
sara Rabiee <sarahrabiee@gmail.com>
2021-02-26 17:29:16 +0100

    mproves the is-prime methode

--- exercises/03-is_prime.py
+++ exercises/03-is_prime.py
@@ -1,12 +1,13 @@
 def is_prime(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
+    if n > 1:
+        for i in range(2, int(n/2)+1):
+            if n % i == 0:
+                return False
+        return True
+    return false
 
 if __name__ == '__main__':
-    state = is_prime(7)
+    state = is_prime(67)
     if state:
         print("Is a prime number")
     else: 
